4d3933984b18bb21b2768f535be4fc403d031a8c
nancy.garcia2 <nancy.garcia2@bbva.com>
2023-10-30 15:26:13 -0600

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from the Mexico City!🇲🇽";
+const hello = () => "Hello World from the Mexico City!🇲🇽... TesT Actividad 4.2";
 console.log(hello());
 exports.hello = hello;
